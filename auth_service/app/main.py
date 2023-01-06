4f5b564c4247c48d5a48797349ddb09fd1df2f12
Parenek67 <kliment207@gmail.com>
2023-01-06 19:16:00 +0300

    бд в 1 сервис

--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -1,23 +1,40 @@
-from fastapi import FastAPI, HTTPException
-from app.users import User
+from fastapi import FastAPI, HTTPException, Depends
+
+from .database import get_db
+from sqlalchemy.orm import Session
+from . import models, schemas, crud
+from .database import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 users_list = [
-    User(0, "Petya", "petya123", "123"),
-    User(1, "Vasya", "vasya456", "456"),
-    User(2, "Anton", "anton789", "789")
+
 ]
 
 app = FastAPI()
 
+@app.post("/v1/postusers")
+async def create(details: schemas.CreateUser, db: Session = Depends(get_db)):
+    to_create = models.User(
+        name = details.name,
+        login = details.login,
+        password = details.password
+    )
+    db.add(to_create)
+    db.commit()
+    return {
+        "success": True,
+        "created_id":to_create.id
+    }
 
 @app.get("/v1/users/{login}/{password}")
-async def auth(login, password):
-    for user in users_list:
-        if user.login == login and user.password == password:
-            return {"message": "Hello " + user.name}
-    raise HTTPException(status_code=404, detail="Error")
+async def auth(login:str, password:str, db: Session = Depends(get_db)):
+    result = crud.auth(db, login, password)
+    if result is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return result
 
 
 @app.get("/v1/users/")
-async def get_users():
-    return users_list
+async def get_users(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
+    return crud.get_users(db, skip=skip, limit=limit)
